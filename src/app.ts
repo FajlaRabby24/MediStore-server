cfb2c3b44e896256d7025efd0fb39f83699a1796
FajlaRabby24 <fajlarabbyla24@gmail.com>
2026-01-31 02:21:37 +0600

    modifie schema

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,21 @@
+import { toNodeHandler } from "better-auth/node";
+import cors from "cors";
 import express, { Request, Response } from "express";
+import { config } from "./config";
+import { auth } from "./lib/auth";
 
 const app = express();
+
+// middleware
+app.use(express.json());
+app.use(
+  cors({
+    origin: config.app_url,
+    credentials: true,
+  }),
+);
+
+app.all("/api/auth/*spalte", toNodeHandler(auth));
 
 app.use("/", (req: Request, res: Response) => {
   res.status(200).json({
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,20 @@
 import app from "./app";
 import { config } from "./config";
+import { prisma } from "./lib/prisma";
 
 const port = config.port;
 
 const main = async () => {
   try {
+    await prisma.$connect();
+    console.log("✅ connected to database successfully");
+
     app.listen(port, () => {
       console.log(`🚀 Server is running on port ${port}`);
     });
   } catch (error) {
     console.error("An error occured", error);
+    await prisma.$disconnect();
     process.exit(1);
   }
 };
